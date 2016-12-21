4346a96cb795e27156dab3a10b586edab86c5c5b
karan1149 <karan1149@gmail.com>
2016-12-20 23:35:07 -0500

    Fixed string-int comparison and casted integers in dictionary

--- scraper.py
+++ scraper.py
@@ -3,6 +3,6 @@ import sys
 
 with open(sys.argv[1], 'rb') as csvfile:
     reader = csv.reader(csvfile);
-    alexaDict = {row[1]: row[0] for row in reader if row[0] < int(sys.argv[2]) + 1};
+    alexaDict = {row[1]: int(row[0]) for row in reader if int(row[0]) < int(sys.argv[2]) + 1};
 
 print alexaDict;
